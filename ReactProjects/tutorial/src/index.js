ac1b659bfa9e64112fc31d82ebf2d7d733199735
jwinter10581 <jwinter10581@gmail.com>
2021-09-15 09:41:10 -0500

    React Tutorial - Basics complete, Onto Advanced

--- ReactProjects/tutorial/src/index.js
+++ ReactProjects/tutorial/src/index.js
@@ -1,29 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-
-const books = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg",
-    title: "The Very Hungry Caterpillar",
-    author: "Eric Carle",
-  },
-  {
-    id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/91m4x8jBBzS._AC_UL200_SR200,200_.jpg",
-    title: "An Unapologetic Cookbook",
-    author: "Joshua Weissman",
-  },
-  {
-    id: 3,
-    img: "https://images-na.ssl-images-amazon.com/images/I/91O%2B9NHn0uL._AC_UL200_SR200,200_.jpg",
-    title: "The Stay-at-Home Chef Slow Cooker Cookbook",
-    author: "Rachel Farnsworth",
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
+import { greeting } from "./testing/testing";
 
 function BookList() {
+  console.log(greeting);
   return (
     <section className="bookList">
       {books.map((book) => {
@@ -32,36 +15,5 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert("hello world");
-  };
-
-  const complexExample = (author) => {
-    console.log(author);
-  };
-
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img}></img>
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        Reference Example
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        More Complex Example
-      </button>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
