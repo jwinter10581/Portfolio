229dd08fd4692a58522e404deb579e7b5ca35816
jwinter10581 <jwinter10581@gmail.com>
2021-09-16 09:09:33 -0500

    React Tutorial - Commit Project Starter Files

---
+++ ReactProjects/react-advanced-tutorial/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
+// import Final from "./tutorial/1-useState/final/3-useState-array";
+
+function App() {
+  return (
+    <div className="container">
+      <Setup />
+      {/* <Final /> */}
+    </div>
+  );
+}
+
+// import Final from "./tutorial/1-useState/final/1-error-example";
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <Final />
+//     </div>
+//   );
+// }
+
+export default App;
